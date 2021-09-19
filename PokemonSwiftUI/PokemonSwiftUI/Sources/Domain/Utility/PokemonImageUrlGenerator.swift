4372f0bd79001afbd52b415c051e761f7044d7fe
Shumpei Nagata <nagata.syunpei@gmail.com>
2021-09-20 02:09:00 +0900

    Lintのエラー対応

--- PokemonSwiftUI/PokemonSwiftUI/Sources/Domain/Utility/PokemonImageUrlGenerator.swift
+++ PokemonSwiftUI/PokemonSwiftUI/Sources/Domain/Utility/PokemonImageUrlGenerator.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 enum PokemonImageURLGenerator {
-    static func generateThumbnailURL(from id: Int) -> URL? {
+    static func generateThumbnailURL(from number: Int) -> URL? {
         // 3桁0埋め文字列生成
-        let formatId = String(format: "%03d", id)
+        let formatId = String(format: "%03d", number)
         let imageUrl = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/thumbnails/\(formatId).png"
         return URL(string: imageUrl)
     }
 
-    static func generateImageURL(from id: Int) -> URL? {
+    static func generateImageURL(from number: Int) -> URL? {
         // 3桁0埋め文字列生成
-        let formatId = String(format: "%03d", id)
+        let formatId = String(format: "%03d", number)
         let imageUrl = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/\(formatId).png"
         return URL(string: imageUrl)
     }
